d7437b89cd7ee916ddfb74efef856c3461728d19
sharmaar342 <sharmaar@bc.edu>
2016-10-18 20:47:20 -0400

    Upload Testing

---
+++ CO2.R
@@ -0,0 +1,15 @@
+
+##Upload code into Github from R through console
+getwd()
+ls()
+
+library(datasets)
+str(CO2)  
+fit<-lm(uptake ~ conc,data = CO2)
+summary(fit)
+str(fit)
+fitted(fit)
+residuals(fit)
+
+plot(CO2$conc, CO2$uptake, main = "Conc by Uptake")
+abline(fit)
